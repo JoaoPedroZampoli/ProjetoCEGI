80cfaa4d8a956ee8b9fc91143a4f47a366a11b84
Victoriasemc1 <alves.vitoria@unifesp.br>
2023-11-18 11:50:50 -0300

    Pulo e andar

--- Assets/BigManJD/Platformer Tileset - Pixelart Grasslands/Character/Script/Moviment.cs
+++ Assets/BigManJD/Platformer Tileset - Pixelart Grasslands/Character/Script/Moviment.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoviment : MonoBehaviour
+public class Moviment : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
@@ -13,24 +13,36 @@ public class PlayerMoviment : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
     }
+
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
     private void Flip()
     {
-        if(isFacingRight && horizontal < 0f || isFacingRight && horizontal > 0f)
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
