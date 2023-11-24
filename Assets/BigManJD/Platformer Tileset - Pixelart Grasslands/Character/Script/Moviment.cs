e477344198aa8163cf5c542353af7aad202259d8
João Pedro Zampoli <jpszampoli53@gmail.com>
2023-11-24 15:58:51 -0300

    Rollback Commit
    Rollback devido a problemas

--- Assets/BigManJD/Platformer Tileset - Pixelart Grasslands/Character/Script/Moviment.cs
+++ Assets/BigManJD/Platformer Tileset - Pixelart Grasslands/Character/Script/Moviment.cs
@@ -4,34 +4,15 @@ using UnityEngine;
 
 public class Moviment : MonoBehaviour
 {
-    //Velocidade 
     private float horizontal;
-<<<<<<< HEAD
-    private float speed = 8f;
-
-    //Pulo
-    private float jumpingPower = 16f;
-=======
     public float speed = 8f;
     public float jumpingPower = 10f;
->>>>>>> 89a46d6117316f9ff1f3b8baaabb96f50ed4f11d
     private bool isFacingRight = true;
-    public bool InFloor;
-    public Transform DetectsGround;
-    public LayerMask Floor;
 
-    //Moviemntação do player
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-    //Animator
-    public Animator animator;
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-
-    }
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -39,7 +20,6 @@ public class Moviment : MonoBehaviour
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            animator.SetBool("Jump", true);
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
@@ -48,30 +28,6 @@ public class Moviment : MonoBehaviour
         }
 
         Flip();
-
-
-        InFloor = Physics2D.OverlapCircle(DetectsGround.position, 0.2f, Floor);
-
-        //Animator
-
-        //Run
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            animator.SetBool("Run", true);
-        }
-        else
-        {
-            animator.SetBool("Run", false);
-        }
-        //jump
-        if (InFloor &&  rb.velocity.y == 0)
-        {
-            animator.SetBool("Jump", false);
-        }
-        if (Input.GetButtonDown("Jump"))
-        {
-            animator.SetBool("Jump", true);
-        }
     }
 
     private void FixedUpdate()
