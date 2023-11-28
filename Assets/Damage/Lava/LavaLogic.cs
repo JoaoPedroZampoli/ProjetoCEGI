02f40819994fbc42d67457acc4bb528df0623046
Victoriasemc1 <alves.vitoria@unifesp.br>
2023-11-28 20:18:12 -0300

    Lava funcionando

--- Assets/Damage/Lava/LavaLogic.cs
+++ Assets/Damage/Lava/LavaLogic.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
 
 public class LavaLogic : MonoBehaviour
 {
-    Moviment player;
-    public bool IsDead;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            IsDead = true;
-            player.animator.SetBool("IsDead", IsDead);
-            GetComponent<Moviment>().enabled = false;
-            Destroy(gameObject, 1.0f);
+            collision.gameObject.GetComponent<Animator>().SetTrigger("IsDead");
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            collision.gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+            collision.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            collision.gameObject.GetComponent<Moviment>().enabled = false;
+            collision.gameObject.GetComponent<Animator>().SetBool("Jump", false);
+            LoadScene();
         }
+    }
+    void LoadScene()
+    {
+        SceneManager.LoadScene("DeathScene");
     }
 }
