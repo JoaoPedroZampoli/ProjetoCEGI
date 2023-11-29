8a6e7f7e0288f04864ab1703cb1a645ff6c48c49
Yagamikk <davilmlima@outlook.com>
2023-11-29 15:38:02 -0300

    adição do background

---
+++ Assets/BigManJD/Platformer Tileset - Pixelart Grasslands/Tile Palette/Tiles/background/Parallax.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length;
+    private float startPos;
+
+    private Transform cam;
+
+    [SerializeField]
+    private float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
