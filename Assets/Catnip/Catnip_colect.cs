1ce0d752da6112a1488748197175d266e335a160
Victoriasemc1 <alves.vitoria@unifesp.br>
2023-12-04 09:58:26 -0300

    Catnip pronto

--- Assets/Catnip/Catnip_colect.cs
+++ Assets/Catnip/Catnip_colect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Catnip_colect : MonoBehaviour
 {
@@ -9,7 +10,11 @@ public class Catnip_colect : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         {
-
+            LoadScene();
         }
+    }
+    void LoadScene()
+    {
+        SceneManager.LoadScene("VictoryScene");
     }
 }
